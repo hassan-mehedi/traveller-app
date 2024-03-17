d6774c4e448e82c497f56894b5c818943366201b
hassan-mehedi <howlader.mehedihassan@gmail.com>
2024-03-17 16:49:45 +0600

    chore: update quote style and content

--- frontend/src/components/quote.js
+++ frontend/src/components/quote.js
@@ -1,20 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Quote() {
     return (
-        <header className=" h-[28rem] bg-gray-50">
-            <div className=" container mx-auto px-4 flex h-full py-6 items-center">
-                <div className=" max-w-xl">
-                    <p className=" text-sky-500 uppercase tracking-wider">knowleddge base</p>
+        <section class=" h-[28rem] bg-gray-50 flex flex-col items-center justify-center">
+            <div class="container flex flex-col items-center px-4 py-12 mx-auto text-center">
+                <h2 class="max-w-2xl mx-auto text-2xl font-semibold tracking-tight text-gray-800 xl:text-3xl ">
+                    Share your experience with the <span class="text-blue-500">whole world.</span>
+                </h2>
 
-                    <h2 className=" text-3xl xl:text-4xl font-bold mt-4 text-gray-800 capitalize">All Resurece You Need to grow</h2>
+                <p class="max-w-4xl mt-6 text-center text-gray-500 ">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum quidem officiis reprehenderit, aperiam veritatis non, quod veniam
+                    fuga possimus hic explicabo laboriosam nam. A tempore totam ipsa nemo adipisci iusto!
+                </p>
 
-                    <p className=" text-gray-500 mt-4 text-lg">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta error odit vel minima optio consequuntur atque omnis,
-                        repellendus eos, rerum totam quas vitae voluptatibus, facere distinctio fugit? Nam, iure expedita!
-                    </p>
+                <div class="inline-flex w-full mt-6 sm:w-auto">
+                    <Link
+                        href="/signup"
+                        class="mr-2 inline-flex items-center justify-center w-full px-6 py-2 text-white duration-300 bg-blue-600 rounded-lg hover:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+                    >
+                        Sign Up
+                    </Link>
                 </div>
             </div>
-        </header>
+        </section>
     );
 }
