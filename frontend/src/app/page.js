13a3a752975d80bb98b10ec6bf6f42b68f17990d
hassan-mehedi <howlader.mehedihassan@gmail.com>
2024-03-17 16:50:06 +0600

    chore: create sign in and sign up pages

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,3 @@
-import NavBar from "@/components/navbar";
 import Quote from "@/components/quote";
 import Blogs from "@/components/blogs";
 import Footer from "@/components/footer";
@@ -6,7 +5,6 @@ import Footer from "@/components/footer";
 export default function Home() {
     return (
         <main>
-            <NavBar />
             <Quote />
             <Blogs />
             <Footer />
