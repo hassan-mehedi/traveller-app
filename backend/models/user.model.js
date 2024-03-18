c450de9f64aa074da7c792be985a1f0bb3604e4c
Md. Mehedi Hassan <howlader.mehedihassan@gmail.com>
2024-03-18 15:13:29 +0600

    chore: add image field in user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
         name: { type: String, required: [true, "Name is required"], trim: true },
         address: { type: String, trim: true },
         phone: { type: String, trim: true, match: /^\d{11,15}$/ },
-        image: { type: String, trim: true },
+        image: { type: String, trim: true, default: null },
         email: { type: String, required: [true, "Email is required"], trim: true, unique: true },
         password: { type: String, required: [true, "password is required"], trim: true },
         followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
